2cd738bdd3512e0a9c99d40cf65ae0b7fc7539fc
anhphan <anh.phan156@protonmail.com>
2024-05-31 22:32:07 -0400

    update wipe script

--- hosts/vmtest/default.nix
+++ hosts/vmtest/default.nix
@@ -29,28 +29,18 @@ inputs.nixpkgs.lib.nixosSystem {
 
 			boot.initrd.postDeviceCommands = lib.mkAfter ''
 				mkdir /btrfs_tmp
-				mount -o subvol=/root /dev/mapper/crypted /btrfs_tmp
+				mkdir /btrfs_tmp/root
+				mkdir /btrfs_tmp/old_roots
+				mount -o subvol=/root /dev/mapper/crypted /btrfs_tmp/root
 
 				if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
         mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
     fi
 
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
-
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
 				btrfs subvolume create /btrfs_tmp/root
-				umount /btrfs_tmp
+				cp -r /btrfs_tmp/old_roots /btrfs_tmp/root
+				umount /btrfs_tmp/root
 			'';
 
       networking.hostName = "vmtest"; # Define your hostname.
