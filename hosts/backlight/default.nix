b734ffa531fd34c5bc5f54fd75ee586738f50370
anhphan <anh.phan156@protonmail.com>
2024-06-07 22:35:29 -0400

    impermanence for backlight

--- hosts/backlight/default.nix
+++ hosts/backlight/default.nix
@@ -28,5 +28,35 @@ with lib; {
     music = enabled;
   };
 
+  fileSystems."/persistence".neededForBoot = true;
+  environment.persistence."/persistence" = {
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+      {
+        directory = "/var/lib/colord";
+        user = "colord";
+        group = "colord";
+        mode = "u=rwx,g=rx,o=";
+      }
+    ];
+    files = [
+      "/etc/machine-id"
+    ];
+    users."${user.name}" = {
+      directories = [
+        "dotfiles"
+        {
+          directory = ".ssh";
+          mode = "0700";
+        }
+      ];
+    };
+  };
+
   system.stateVersion = "23.11";
 }
