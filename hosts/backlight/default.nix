081764b7b34339aa283e95a75a47941738ba7bb7
anhphan <anh.phan156@protonmail.com>
2024-06-18 10:48:50 -0400

    .mozilla persistence magically worked

--- hosts/backlight/default.nix
+++ hosts/backlight/default.nix
@@ -43,6 +43,7 @@ with lib; {
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
+      "/var/db/sudo/lectured"
       "/etc/NetworkManager/system-connections"
       {
         directory = "/var/lib/colord";
@@ -59,6 +60,7 @@ with lib; {
         "dotfiles"
         "data"
         ".ssh"
+        ".mozilla"
       ];
     };
   };
