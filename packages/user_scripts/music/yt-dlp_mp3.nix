7d109671250933d8955b36dd1942afe3d127ae02
anhphan <anh.phan156@protonmail.com>
2024-07-12 06:56:09 -0400

    starship

--- packages/user_scripts/music/yt-dlp_mp3.nix
+++ packages/user_scripts/music/yt-dlp_mp3.nix
@@ -2,5 +2,13 @@
 pkgs.writeShellScriptBin "music_download" ''
   #!/usr/bin/env bash
 
-  ${pkgs.yt-dlp}/bin/yt-dlp -x --audio-format mp3 -o "~/Downloads/$2.%(ext)s" $1
+	output=~/data/Music/$2.mp3
+
+  ${pkgs.yt-dlp}/bin/yt-dlp -x --audio-format mp3 -o $output $1
+
+	if [ $? -eq 0 ]; then
+		${pkgs.id3v2}/bin/id3v2  -a "$3" -t "$4" $output
+	else
+		echo "Failed to download from yt"
+	fi
 ''
