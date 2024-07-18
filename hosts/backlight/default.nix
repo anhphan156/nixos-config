b5daf00112a3e1b274b9520ab87d3526e97e7f6c
anhphan <anh.phan156@protonmail.com>
2024-07-17 20:12:36 -0400

    misc

--- hosts/backlight/default.nix
+++ hosts/backlight/default.nix
@@ -70,6 +70,7 @@ with lib; {
         ".ssh"
         ".mozilla"
         ".rustup"
+        ".cargo"
       ];
     };
   };
--- packages/user_scripts/tmux_code_layout.nix
+++ packages/user_scripts/tmux_code_layout.nix
@@ -1,12 +1,14 @@
 {pkgs}:
 pkgs.writeShellScriptBin "tmux_code" ''
-   #!/usr/bin/env bash
+  #!/usr/bin/env bash
 
-	tmux kill-session -t Dev
-  tmux new-session -d -s Dev
-  tmux send-keys -t Dev " nvim" Enter
-  tmux split-window -v
-  tmux select-pane -t 0
-  tmux resize-pane -D 6
-  tmux attach -t Dev
+  SESSION_NAME="''${1:-Dot}"
+
+   tmux kill-session -t $SESSION_NAME
+   tmux new-session -d -s $SESSION_NAME
+   tmux send-keys -t $SESSION_NAME " nvim" Enter
+   tmux split-window -v
+   tmux select-pane -t 0
+   tmux resize-pane -D 6
+   tmux attach -t $SESSION_NAME
 ''
