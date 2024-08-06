bd614c9b218f24e82e74dbf9afdb71add37c79f5
anhphan <anh.phan156@protonmail.com>
2024-08-05 23:44:24 -0400

    xremap mapping

--- modules/system/Xremap/default.nix
+++ modules/system/Xremap/default.nix
@@ -4,29 +4,28 @@
   lib,
   ...
 }: {
-	imports = [
-		({
-			services.xremap.enable = lib.mkForce config.cyanea.system.xremap.enable;
-		})
-	];
 
   options.cyanea.system.xremap.enable = lib.mkEnableOption "Enable Xremap";
 
-  config = lib.mkIf config.cyanea.system.xremap.enable {
-    services.xremap = {
-      withWlroots = config.cyanea.graphical.hyprland.enable;
-      userName = user.name;
-      config = {
-        modmap = [
-          {
-            name = "Key remap";
-            remap = {
-              CONTROL_L = "CAPSLOCK";
-              CAPSLOCK = "CONTROL_L";
-            };
-          }
-        ];
+  config = 
+    {
+      services.xremap.enable = lib.mkForce config.cyanea.system.xremap.enable;
+    } //
+    lib.mkIf config.cyanea.system.xremap.enable {
+      services.xremap = {
+        withWlroots = config.cyanea.graphical.hyprland.enable;
+        userName = user.name;
+        config = {
+          modmap = [
+            {
+              name = "Key remap";
+              remap = {
+                CONTROL_L = "Esc";
+                CAPSLOCK = "CONTROL_L";
+              };
+            }
+          ];
+        };
       };
     };
-  };
 }
