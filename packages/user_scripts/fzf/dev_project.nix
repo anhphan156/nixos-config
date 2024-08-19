3d7d5001f314fb6575e18a5681fc47e900e63ccf
anhphan <anh.phan156@protonmail.com>
2024-08-19 12:20:13 -0400

    misc

--- packages/user_scripts/fzf/dev_project.nix
+++ packages/user_scripts/fzf/dev_project.nix
@@ -1,10 +1,11 @@
-{ pkgs } : pkgs.writeShellScriptBin "dev_fzf" ''
-#!/usr/bin/env bash
+{pkgs}:
+pkgs.writeShellScriptBin "dev_fzf" ''
+  #!/usr/bin/env bash
 
-base=~/data/dev
-dir=$(ls $base | fzf)
-if [ $? -eq 0 ]; then
-	cd $base/$dir
-	tmux_code $dir
-fi
+  base=~/data/dev
+  dir=$(ls $base | fzf --preview "ls $base/{}")
+  if [ $? -eq 0 ]; then
+  	cd $base/$dir
+  	tmux_code $dir
+  fi
 ''
