549d80d472c67e283d974c0ce125f87ca78d967f
anhphan <anh.phan156@protonmail.com>
2024-12-16 00:06:09 -0500

    update

--- modules/services/water_reminder.nix
+++ modules/services/water_reminder.nix
@@ -17,7 +17,7 @@
     };
     systemd.services."water-reminder" = {
       script = ''
-        ${pkgs.libnotify}/bin/notify-send "Reminder" "Drink yo watah" -t 30000 --icon="/home/backspace/dotfiles/config/awesome/themes/default/icons/rain.png"
+        ${pkgs.libnotify}/bin/notify-send "Reminder" "Drink yo watah" -t 30000 --icon="${config.cyanea.wallpapers}/icons/rain.png"
       '';
       serviceConfig = {
         Type = "oneshot";
