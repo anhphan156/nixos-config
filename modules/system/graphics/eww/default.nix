ced4ad03a94d184009d389acd613acc9ef23ee81
backspace <anh.phan156@protonmail.com>
2024-12-18 11:25:41 -0500

    eww update

--- modules/system/graphics/eww/default.nix
+++ modules/system/graphics/eww/default.nix
@@ -8,7 +8,9 @@
   options.cyanea.graphical.eww.enable = lib.mkEnableOption "Enable Eww";
 
   config = lib.mkIf config.cyanea.graphical.eww.enable {
-    home-manager.users."${lib.user.name}" = {
+    home-manager.users."${lib.user.name}" = let
+      eww-config = inputs.eww-config.packages.${pkgs.system}.default;
+    in {
       # programs.eww = {
       #   enable = true;
       #   configDir = dotfilesPath + /config/eww;
@@ -16,10 +18,11 @@
 
       home.packages = with pkgs; [
         eww
+        eww-config
       ];
 
       xdg.configFile = {
-        "eww/".source = inputs.eww-config.packages.${pkgs.system}.default;
+        "eww/".source = "${eww-config}/share/eww-config";
       };
     };
   };
