7c3c5b61456bffa4d6c4e598f3068502b28ce7f1
backspace <anh.phan156@protonmail.com>
2024-12-18 11:03:49 -0500

    update

--- modules/system/graphics/eww/default.nix
+++ modules/system/graphics/eww/default.nix
@@ -4,34 +4,20 @@
   pkgs,
   inputs,
   ...
-}: let
-  # TODO make a c program to keep track of what window is open and closed with hyprland ipc and emits an output for (deflisten)
-  leftdockscript = pkgs.writeShellScriptBin "leftdockcheck" ''
-    #!/usr/bin/env bash
-
-    eww update revealdock=true
-
-    hyprctl clients | grep "class: firefox"
-
-    if [[ $? -eq 0 ]]; then
-      eww update icon1=true
-    else
-      eww update icon1=false
-    fi
-  '';
-in {
+}: {
   options.cyanea.graphical.eww.enable = lib.mkEnableOption "Enable Eww";
 
   config = lib.mkIf config.cyanea.graphical.eww.enable {
-    environment.systemPackages = with pkgs; [
-      eww
-      leftdockscript
-    ];
     home-manager.users."${lib.user.name}" = {
       # programs.eww = {
       #   enable = true;
       #   configDir = dotfilesPath + /config/eww;
       # };
+
+      home.packages = with pkgs; [
+        eww
+      ];
+
       xdg.configFile = {
         "eww/".source = inputs.eww-config.packages.${pkgs.system}.default;
       };
