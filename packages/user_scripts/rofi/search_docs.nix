f93ecf1e5c541bf3db21778f0419debc019739db
backspace <anh.phan156@protonmail.com>
2024-12-21 20:29:00 -0500

    misc

--- packages/user_scripts/rofi/search_docs.nix
+++ packages/user_scripts/rofi/search_docs.nix
@@ -4,12 +4,13 @@
   rofi-wayland,
   evince,
   libreoffice,
+  dotfilesPath,
   ...
 }:
 writeShellScriptBin "search_docs" ''
   #!/usr/bin/env bash
 
-  arg=$(find ~ 2> /dev/null | ${rofi-wayland}/bin/rofi -i -dmenu -p "Select a document:" -config ${rootPath + /config/rofi/config1Col.rasi})
+  arg=$(find ~ 2> /dev/null | ${rofi-wayland}/bin/rofi -i -dmenu -p "Select a document:" -config ${dotfilesPath + /rofi/config1Col.rasi})
 
   filename=$(basename $arg)
   extension="''${filename##*.}"
