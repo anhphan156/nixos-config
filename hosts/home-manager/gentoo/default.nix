f76167193b3e832e37936adf09ea07de19ba0ae2
backspace <anh.phan156@protonmail.com>
2025-06-18 02:54:30 -0400

    librewolf for gentoo hm

--- hosts/home-manager/gentoo/default.nix
+++ hosts/home-manager/gentoo/default.nix
@@ -10,6 +10,7 @@
     "${inputs.self}/modules/home-manager/dotfiles/git"
     "${inputs.self}/modules/home-manager/dotfiles/rofi"
     "${inputs.self}/modules/home-manager/dotfiles/kitty"
+    "${inputs.self}/modules/home-manager/dotfiles/tmux"
     "${inputs.self}/modules/home-manager/dotfiles/fastfetch"
     "${inputs.self}/modules/home-manager/dotfiles/shell/zsh.nix"
     "${inputs.self}/modules/home-manager/dotfiles/shell/starship.nix"
@@ -17,11 +18,38 @@
 
   programs.fastfetch.settings.logo = lib.mkForce "${pkgs.wallpapers}/fetch_logo/sw.png";
 
+  programs.zsh.shellAliases = {
+    "sv" = "sudo /home/${lib.user.name}/.nix-profile/bin/nvim";
+  };
+
+  programs.direnv = {
+    enable = true;
+    silent = true;
+  };
+
+  gtk.enable = true;
+
+  gtk.theme.package = pkgs.tokyonight-gtk-theme;
+  gtk.theme.name = "Tokyonight-Dark";
+
+  gtk.iconTheme.package = pkgs.paper-icon-theme;
+  gtk.iconTheme.name = "Paper";
+
   home.packages = with pkgs; [
     neovim
+    librewolf
+    discord
     (callPackage "${inputs.self}/packages/scripts/rofi/text_clipboard.nix" {
       rofiPromptConfig = config.dotfiles.rofi.prompt;
       rofiImgConfig = config.dotfiles.rofi.image;
+    })
+    (callPackage "${inputs.self}/packages/scripts/dev/devgui.nix" {
+      basePath = lib.user.path.dev;
+      tmux_code = callPackage "${inputs.self}/packages/scripts/dev/tmux_code_layout.nix" {};
+      rofiConfig = config.dotfiles.rofi.default;
+    })
+    (callPackage "${inputs.self}/packages/scripts/rofi/search_docs.nix" {
+      rofiConfig = config.dotfiles.rofi.oneColumn;
     })
   ];
 }
